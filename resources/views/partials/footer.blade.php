1b686e2f5bd6058753b8c3e683d99adbdc333ec0
EmanuelaPau <uapame@gmail.com>
2023-07-19 18:35:43 +0200

    Add footer links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,42 @@
 <footer>
-    <h1>
-        this is a footer
-    </h1>
+    <div class="footer-nav">
+
+        <div class="container">
+            <div class="nav-menu">
+
+                @foreach ($footerLinks['navLinks'] as $link)
+                <div class="ul-list-container">
+                    <h3>{{ $link['title'] }}</h3>
+                    <ul>
+                        @foreach ($link['pages'] as $page)
+                            <li>
+                                <a href="">
+                                    {{$page}}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endforeach
+            </div>
+
+            <img src="../assets/dc-logo-bg.png" alt="">
+        </div>
+
+    </div>
+    <div class="footer-cta container">
+        <button class="button-cta">Sign-up now!</button>
+
+        {{-- <nav>
+            <a class="cta-txt" href="">
+                follow us
+            </a>
+
+            <a v-for="icons in navSocials" href="">
+                <img :src="'../assets/' + icons.img" :alt="icons.link">
+            </a>
+        </nav> --}}
+
+    </div>
 </footer>
+</template>
